1b3b62258dc4ee99c4b02bade8873118fae19537
tom.norris@freedomtoinsure.co.uk <tom.norris@freedomtoinsure.co.uk>
2021-04-19 17:47:30 +0100

    Refactored parts of the html

--- ApplicationProcessor/Application.cs
+++ ApplicationProcessor/Application.cs
@@ -36,23 +36,27 @@ namespace ULaw.ApplicationProcessor
         {
             var result = new StringBuilder("<html><body><h1>Your Recent Application from the University of Law</h1>");
 
+            AddIntro(result);
+
             if (DegreeGrade == DegreeGradeEnum.twoTwo)
             {
-                result.Append(string.Format("<p> Dear {0}, </p>", FirstName));
-                result.Append(string.Format("<p/> Further to your recent application for our course reference: {0} starting on {1}, we are writing to inform you that we are currently assessing your information and will be in touch shortly.", this.CourseCode, this.StartDate.ToLongDateString()));
+                // Add Body
+                result.Append(string.Format(" for our course reference: {0} starting on {1}, we are writing to inform you that we are currently assessing your information and will be in touch shortly.", this.CourseCode, this.StartDate.ToLongDateString()));
                 result.Append("<br/> If you wish to discuss any aspect of your application, please contact us at AdmissionsTeam@Ulaw.co.uk.");
-                result.Append("<br/> Yours sincerely,");
-                result.Append("<p/> The Admissions Team,");
+
+                // Could be done in AddSignature with bool to decide if html does not need "/"
+                result.Append("<br/>");
             }
             else
             {
                 if (DegreeGrade == DegreeGradeEnum.third)
                 {
-                    result.Append(string.Format("<p> Dear {0}, </p>", FirstName));
-                    result.Append("<p/> Further to your recent application, we are sorry to inform you that you have not been successful on this occasion.");
+                    // Add Body
+                    result.Append(", we are sorry to inform you that you have not been successful on this occasion.");
                     result.Append("<br/> If you wish to discuss the decision further, or discuss the possibility of applying for an alternative course with us, please contact us at AdmissionsTeam@Ulaw.co.uk.");
-                    result.Append("<br> Yours sincerely,");
-                    result.Append("<p/> The Admissions Team,");
+
+                    // Could be done in AddSignature with bool to decide if html does not need "/"
+                    result.Append("<br>");
                 }
                 else
                 {
@@ -60,30 +64,45 @@ namespace ULaw.ApplicationProcessor
                     {
                         decimal depositAmount = 350.00M;
 
-                        result.Append(string.Format("<p> Dear {0}, </p>", FirstName));
-                        result.Append(string.Format("<p/> Further to your recent application, we are delighted to offer you a place on our course reference: {0} starting on {1}.", this.CourseCode, this.StartDate.ToLongDateString()));
+                        // Add Body
+                        result.Append(string.Format(", we are delighted to offer you a place on our course reference: {0} starting on {1}.", this.CourseCode, this.StartDate.ToLongDateString()));
                         result.Append(string.Format("<br/> This offer will be subject to evidence of your qualifying {0} degree at grade: {1}.", DegreeSubject.ToDescription(), DegreeGrade.ToDescription()));
                         result.Append(string.Format("<br/> Please contact us as soon as possible to confirm your acceptance of your place and arrange payment of the £{0} deposit fee to secure your place.", depositAmount.ToString()));
                         result.Append(string.Format("<br/> We look forward to welcoming you to the University,"));
-                        result.Append(string.Format("<br/> Yours sincerely,"));
-                        result.Append(string.Format("<p/> The Admissions Team,"));
+
+                        // Could be done in AddSignature with bool to decide if html does not need "/"
+                        result.Append("<br/>");
                     }
                     else
                     {
-                        result.Append(string.Format("<p> Dear {0}, </p>", FirstName));
-                        result.Append(string.Format("<p/> Further to your recent application for our course reference: {0} starting on {1}, we are writing to inform you that we are currently assessing your information and will be in touch shortly.", this.CourseCode, this.StartDate.ToLongDateString()));
+                        // Add Body
+                        result.Append(string.Format(" for our course reference: {0} starting on {1}, we are writing to inform you that we are currently assessing your information and will be in touch shortly.", this.CourseCode, this.StartDate.ToLongDateString()));
                         result.Append("<br/> If you wish to discuss any aspect of your application, please contact us at AdmissionsTeam@Ulaw.co.uk.");
-                        result.Append("<br/> Yours sincerely,");
-                        result.Append("<p/> The Admissions Team,");
+
+                        // Could be done in AddSignature with bool to decide if html does not need "/"
+                        result.Append("<br/>");
                     }
                 }
             }
 
-             result.Append(string.Format("</body></html>"));
+            AddSignature(result);
+
+            result.Append(string.Format("</body></html>"));
 
             return result.ToString();
         }
 
+        private void AddIntro(StringBuilder result)
+        {
+            result.Append(string.Format("<p> Dear {0}, </p>", FirstName));
+            result.Append("<p/> Further to your recent application");
+        }
+
+        private void AddSignature(StringBuilder result)
+        {
+            result.Append(" Yours sincerely,");
+            result.Append("<p/> The Admissions Team,");
+        }
     }
 }
 
